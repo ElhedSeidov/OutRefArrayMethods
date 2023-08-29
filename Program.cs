18fc3ba140cdf81fa640553499399d8526545f8f
reshadsaidov <alhadss@code.edu.az>
2023-08-29 04:41:34 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp6
+{
+
+
+    public class Example
+    {
+
+
+        public static bool CustomTryParse(string a, out int result)
+        {
+
+            string[] strings = a.Split('1','2','3','4','5','6','7','8','9','0');
+            string str = String.Join(null,strings);
+            //Console.WriteLine(str.Length);
+            //foreach (string s in strings) 
+            //{ 
+            //    Console.WriteLine(s);
+            //}
+
+            
+
+            if (str.Length > 0) 
+            {
+                result = 0;
+                return false;
+               
+                
+            }
+            result=int.Parse(a);
+            return true;    
+
+        }
+
+
+        public static void Main()
+        {
+
+            Console.WriteLine(CustomTryParse("67",out int result));
+            Console.WriteLine(result);
+            Console.WriteLine(result*2);
+
+
+
+        }
+    }
+
+}
+    
+ 
